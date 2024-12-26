1f63f8ea25f22264bee571fdeddd76d2943738e6
Lucas Rios <lucasfaesa@gmail.com>
2024-12-26 00:52:30 -0300

    initial commit

---
+++ Source/Game/Code/ShipMovement.cpp
@@ -0,0 +1,61 @@
+﻿#include "ShipMovement.h"
+
+
+
+ShipMovement::ShipMovement(const SpawnParams& params)
+    : Script(params)
+{
+    // Enable ticking OnUpdate function
+    _tickUpdate = true;
+}
+
+
+void ShipMovement::OnEnable()
+{
+    if (character_controller)
+    {
+		DebugLog::LogWarning(TEXT("Character controller is not assigned."));
+        _tickUpdate = false;
+    }
+        
+}
+
+void ShipMovement::OnDisable()
+{
+    
+}
+
+void ShipMovement::OnUpdate()
+{
+    input_reading();
+    move(movement_vector_, ship_speed);
+}
+
+void ShipMovement::input_reading()
+{
+    const float horizontal_value = Input::GetAxis(INPUT_HORIZONTAL);
+    const float depth_value = Input::GetAxis(INPUT_VERTICAL);
+    float altitude_value = 0.0f;
+
+    if (Input::GetActionState(INPUT_INCREASE_ALTITUDE) == InputActionState::Pressing)
+    {
+        altitude_value += 1.0f;
+    }
+    if (Input::GetActionState(INPUT_DECREASE_ALTITUDE) == InputActionState::Pressing)
+    {
+        altitude_value -= 1.0f;
+    }
+
+    movement_vector_ = Vector3(horizontal_value, altitude_value, depth_value);
+
+    movement_vector_.Normalize();
+    
+}
+
+void ShipMovement::move(const Vector3& direction, const float& speed) const
+{
+    character_controller->Move(direction * speed);
+}
+
+
+
---
+++ Source/Game/Code/ShipMovement.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "Engine/Scripting/Script.h"
+#include "Engine/Scripting/ScriptingObjectReference.h"
+#include "Engine/Debug/DebugLog.h"
+#include "Engine/Input/Input.h"
+#include "Engine/Physics/Colliders/CharacterController.h"
+
+constexpr auto INPUT_HORIZONTAL = TEXT("Horizontal");
+constexpr auto INPUT_VERTICAL = TEXT("Vertical");
+constexpr auto INPUT_INCREASE_ALTITUDE = TEXT("IncreaseAltitude");
+constexpr auto INPUT_DECREASE_ALTITUDE = TEXT("DecreaseAltitude");
+
+
+API_CLASS() class GAME_API ShipMovement : public Script
+{
+API_AUTO_SERIALIZATION();
+DECLARE_SCRIPTING_TYPE(ShipMovement);
+
+	API_FIELD() ScriptingObjectReference<CharacterController> character_controller = nullptr;
+    API_FIELD() float ship_speed = 10.f;
+
+    void OnEnable() override;
+    void OnDisable() override;
+    void OnUpdate() override;
+
+private:
+    void input_reading();
+    void move(const Vector3& direction, const float& speed) const;
+
+private:
+    Vector3 movement_vector_ = Vector3::Zero;
+};
