552d2fc34b2dd08f27c4f9e74231c69bd0f14bd9
Lucas Rios <lucasfaesa@gmail.com>
2025-03-18 20:28:22 -0300

    Created universe reset to center

---
+++ Source/Game/Code/5 - World/UniverseToCenter.cpp
@@ -0,0 +1,54 @@
+﻿#include "UniverseToCenter.h"
+
+#include "Engine/Debug/DebugLog.h"
+#include "Engine/Level/Actor.h"
+#include "Engine/Particles/ParticleEffect.h"
+
+UniverseToCenter::UniverseToCenter(const SpawnParams& params)
+    : Script(params)
+{
+    // Enable ticking OnUpdate function
+    _tickUpdate = false;
+}
+
+void UniverseToCenter::OnStart()
+{
+    if (!shipActor && !universeActor)
+    {
+        DebugLog::Log(TEXT("Could not find ship actor"));
+        _tickUpdate = false;
+    }
+	else
+    {
+        _tickUpdate = true;
+    }
+}
+
+void UniverseToCenter::OnEnable()
+{
+    // Here you can add code that needs to be called when script is enabled (eg. register for events)
+}
+
+void UniverseToCenter::OnDisable()
+{
+    // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+}
+
+void UniverseToCenter::OnUpdate()
+{
+    DebugLog::Log(String::Format(TEXT("pos {}"), shipActor->GetPosition().Length()));
+
+    Vector3 currentUniversePos = universeActor->GetPosition();
+    Vector3 shipWorldPos = shipActor->GetPosition();
+
+    if (shipWorldPos.Length() >= distanceThreshold)
+    {
+        DebugLog::Log(TEXT("Reset"));
+        universeActor->SetPosition(currentUniversePos - shipWorldPos);
+
+        for (auto& trail : shipTrails)
+        {
+			trail->ResetSimulation();
+        }
+    }
+}
---
+++ Source/Game/Code/5 - World/UniverseToCenter.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "Engine/Scripting/Script.h"
+#include "Engine/Scripting/ScriptingObjectReference.h"
+
+class ParticleEffect;
+API_CLASS() class GAME_API UniverseToCenter : public Script
+{
+API_AUTO_SERIALIZATION();
+DECLARE_SCRIPTING_TYPE(UniverseToCenter);
+
+
+	API_FIELD(Attributes = "EditorDisplay(\"References\")") ScriptingObjectReference<Actor> universeActor;
+	API_FIELD(Attributes = "EditorDisplay(\"References\")") ScriptingObjectReference<Actor> shipActor;
+    API_FIELD(Attributes = "EditorDisplay(\"References\")") Array<ScriptingObjectReference<ParticleEffect>> shipTrails;
+
+    API_FIELD(Attributes = "EditorDisplay(\"Data\")") float distanceThreshold = 25000.f;
+
+    // [Script]
+    void OnStart() override;
+    void OnEnable() override;
+    void OnDisable() override;
+    void OnUpdate() override;
+
+private:
+    StringView  trail_emitter = TEXT("Trail");
+    StringView  trail_spawn_rate_parameter = TEXT("SpawnRate");
+    StringView  trail_lifetime_parameter = TEXT("Lifetime");
+};
