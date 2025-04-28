e8a2e5f069027b8a921c3a418d7a1abfa8b460f4
Lucas Rios <lucasfaesa@gmail.com>
2025-04-27 21:37:41 -0300

    very barebones implementation of raycasting

---
+++ Source/Game/Code/1 - Player/ShipCombat.cpp
@@ -0,0 +1,65 @@
+﻿#include "ShipCombat.h"
+
+#include "Engine/Debug/DebugDraw.h"
+#include "Engine/Debug/DebugLog.h"
+#include "Engine/Input/Input.h"
+#include "Engine/Level/Actor.h"
+#include "Engine/Physics/Physics.h"
+#include "Engine/Physics/Types.h"
+#include "Engine/Physics/Actors/PhysicsColliderActor.h"
+
+ShipCombat::ShipCombat(const SpawnParams& params)
+    : Script(params)
+{
+    // Enable ticking OnUpdate function
+    _tickUpdate = true;
+}
+
+void ShipCombat::OnEnable()
+{
+
+}
+
+void ShipCombat::OnDisable()
+{
+
+}
+
+void ShipCombat::OnUpdate()
+{
+    
+
+    InputReading();
+}
+
+void ShipCombat::InputReading()
+{
+    if (Input::GetActionState(INPUT_FIRE) == InputActionState::Pressing)
+    {
+        canShoot_ = true;
+        Shoot();
+    }
+}
+
+void ShipCombat::Shoot()
+{
+    DebugLog::Log(TEXT("Try Shoot"));
+
+    if (!canShoot_)
+        return;
+
+    DebugLog::Log(TEXT("Shoot"));
+
+    RayCastHit hit;
+    if (Physics::RayCast(shootingReference->GetPosition(), shootingReference->GetDirection(), hit, MAX_float, layers_to_hit))
+    {
+        
+        DEBUG_DRAW_SPHERE(BoundingSphere(hit.Point, 50), Color::Red, 0.0f, true);
+        DEBUG_DRAW_LINE(shootingReference->GetPosition(), hit.Point, Color::Green, 0.0f, true);
+
+        hit.Collider->GetParent()->SetIsActive(false);
+    }
+    
+
+    canShoot_ = false;
+}
---
+++ Source/Game/Code/1 - Player/ShipCombat.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "Engine/Core/Types/LayersMask.h"
+#include "Engine/Scripting/Script.h"
+#include "Engine/Scripting/ScriptingObjectReference.h"
+
+constexpr auto INPUT_FIRE = TEXT("Fire");
+
+API_CLASS() class GAME_API ShipCombat : public Script
+{
+API_AUTO_SERIALIZATION();
+DECLARE_SCRIPTING_TYPE(ShipCombat);
+
+	API_FIELD(Attributes) ScriptingObjectReference<Actor> shootingReference = nullptr;
+    API_FIELD() LayersMask layers_to_hit;
+
+    // [Script]
+    void OnEnable() override;
+    void OnDisable() override;
+    void OnUpdate() override;
+
+private:
+    bool canShoot_ = true;
+    void InputReading();
+    void Shoot();
+};
